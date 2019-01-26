55ffd2e6d548d8a0cbbc18db43b311fb0706aae1
Junaid Farooq <junaid@evercam.io>
2019-01-26 09:13:24 +0500

    Initial Commit.

---
+++ lib/evercam_models.ex
@@ -0,0 +1,18 @@
+defmodule EvercamModels do
+  @moduledoc """
+  Documentation for EvercamModels.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EvercamModels.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/evercam_models_test.exs
@@ -0,0 +1,8 @@
+defmodule EvercamModelsTest do
+  use ExUnit.Case
+  doctest EvercamModels
+
+  test "greets the world" do
+    assert EvercamModels.hello() == :world
+  end
+end
