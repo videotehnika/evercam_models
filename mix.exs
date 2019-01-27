e930cb55869d268319337b96dd33cd852e5c5f69
Junaid Farooq <junaid@evercam.io>
2019-01-27 09:16:41 +0500

    Added deps. {ecto_sql, postgrex, geo, geo_postgis, poison, calendar, comeonin, uuid}

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule EvercamModels.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :ecto_sql, :postgrex],
       mod: {EvercamModels.Application, []}
     ]
   end
@@ -22,8 +22,14 @@ defmodule EvercamModels.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto_sql, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:geo, "~> 3.0"},
+      {:geo_postgis, "~> 3.0"},
+      {:poison, "~> 3.1"},
+      {:calendar, "~> 0.17.4"},
+      {:comeonin, "~> 3.0.2"},
+      {:uuid, "~> 1.1.7"}
     ]
   end
 end
