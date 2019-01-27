7a2c16812ee237d130dcbc06533f4d57ace62665
Junaid Farooq <junaid@evercam.io>
2019-01-27 09:17:53 +0500

    Added Repo & Snapshot Repo with dev and prod env.

---
+++ config/prod.exs
@@ -0,0 +1,19 @@
+use Mix.Config
+
+config :evercam_models, Evercam.Repo,
+  types: Evercam.PostgresTypes,
+  url: System.get_env("DATABASE_URL"),
+  socket_options: [keepalive: true],
+  timeout: 60_000,
+  pool_size: 80,
+  lazy: false,
+  ssl: true
+
+config :evercam_models, Evercam.SnapshotRepo,
+  url: System.get_env("DATABASE_SNAPSHOT_URL"),
+  ownership_timeout: 60_000,
+  socket_options: [keepalive: true],
+  timeout: 60_000,
+  pool_size: 100,
+  lazy: false,
+  ssl: true
